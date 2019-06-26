c9d095a1166f8d0aefa2fdda0ba57aca8ddf89da
nadavsinai <ns@nadavsinai.com>
2019-06-26 13:10:49 +0300

    basic interfaces

--- src/app/shared/interfaces/incident.interface.ts
+++ src/app/shared/interfaces/incident.interface.ts
@@ -1,7 +1,48 @@
-export type Location = [number,number];
-export interface Incident{
- location:Location;
- reportingTime:number;
- reporter:string;
+export type Location = firebase.firestore.GeoPoint;
+export type TimeStamp = firebase.firestore.Timestamp;
+
+export enum Severity {
+  low = 'low',
+  medium = 'medium',
+  high = 'high',
+  fatal = 'fatal'
 }
 
+
+export interface Incident {
+  location: Location;
+  reportingTime: TimeStamp
+  casualties: Casualty[];
+}
+
+export interface Casualty {
+  id: string;
+  location: Location;
+  treatments: Treatment[];
+}
+
+export enum Status {
+  field = 'field',
+  inTransport = 'inTransport',
+  evacuated = 'evacuated',
+  inHospital = 'inHospital'
+}
+
+export interface Treatment {
+  location: firebase.firestore.GeoPoint;
+  timestamp: firebase.firestore.Timestamp;
+  severity: Severity;
+  reportedBy: firebase.firestore.DocumentReference, // User
+  treatmentNotes: string[];
+  status: Status;
+}
+
+export enum Role {
+  admin = 'admin',
+  medic = 'medic',
+  reportingUser = 'reportingUser'
+}
+
+export interface User extends firebase.User {
+  roles: Role[]
+}
