eff4fc9b5f58fca78fffd58f5008bc56ece0e0bb
nadavsinai <ns@nadavsinai.com>
2019-06-27 13:22:32 +0300

    table stuff

--- src/app/pages/admin/table/table.component.ts
+++ src/app/pages/admin/table/table.component.ts
@@ -1,28 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Time } from '@angular/common';
-import { Incident, Status, Severity } from '../../../shared/interfaces/incident.interface';
-
-export interface TableElement {
-  id: number;
-  status: string;
-  severity: string;
-  nextTreatmentIn: number; //timestamp
-  treatment: string;
-}
-
-const ELEMENT_DATA: TableElement[] = [
-  {id: 1, status: Status.field, severity: Severity.low, nextTreatmentIn: 0, treatment: 'band hand'},
-  {id: 2, status: Status.inHospital, severity: Severity.high, nextTreatmentIn: 15, treatment: 'heart'},
-  {id: 3, status: Status.inTransport, severity: Severity.high, nextTreatmentIn: 30, treatment: 'head'},
-  {id: 4, status: Status.evacuated, severity: Severity.medium, nextTreatmentIn: 60, treatment: 'leg' },
-  {id: 5, status: Status.evacuated, severity: Severity.low, nextTreatmentIn: 0, treatment: 'leg' },
-  {id: 6, status: Status.field, severity: Severity.fatal, nextTreatmentIn: 5, treatment: 'heart' },
-  {id: 7, status: Status.field, severity: Severity.low, nextTreatmentIn: 0, treatment: 'bla bla'},
-  {id: 8, status: Status.inHospital, severity: Severity.low, nextTreatmentIn: 0, treatment: 'leg'},
-  {id: 9, status: Status.inHospital, severity: Severity.medium, nextTreatmentIn: 30, treatment: 'waiting for next mesurments' },
-  {id: 10, status: Status.inTransport, severity: Severity.high, nextTreatmentIn: 0, treatment: 'waiting'},
-];
-
+import {Component, Input, OnInit} from '@angular/core';
+import {Time} from '@angular/common';
+import {Incident, Status, Severity, Casualty} from '../../../shared/interfaces/incident.interface';
 
 @Component({
   selector: 'app-table',
@@ -32,13 +10,17 @@ const ELEMENT_DATA: TableElement[] = [
 
 
 export class TableComponent implements OnInit {
+  @Input() casualties: Casualty[];
+  displayedColumns: string[] = ['id', 'status', 'severity', 'treatmentNotes']; //nextTreatmentIn
 
-  displayedColumns: string[] = ['id', 'status', 'severity', 'nextTreatmentIn', 'treatment'];
-  dataSource = ELEMENT_DATA;
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(){
+    console.log(this.casualties);
   }
 
 }
